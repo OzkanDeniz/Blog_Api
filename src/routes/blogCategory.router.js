98ad58b34c78dcf5409361a8816a8eb4e1919751
ozkan <ozkandnz09@gmail.com>
2024-12-05 23:31:23 +0300

    blogcategoryrouter

---
+++ src/routes/blogCategory.router.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const router = require("express").Router();
+const { blogCategory } = require("../controllers/blogCategory.controller");
+
+//Call Controllers:
+
+// /blog/category/
+router.route("/category").get(blogCategory.list).post(blogCategory.create);
+
+// /blog/category/id
+router
+  .route("/:categoryId")
+  .get(blogCategory.read)
+  .put(blogCategory.update)
+  .patch(blogCategory.update)
+  .delete(blogCategory.delete);
+
+module.exports = router
