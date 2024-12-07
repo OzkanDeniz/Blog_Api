59516d7cd62b8476b73e74f1a3291b6b6fe6b09a
ozkan <ozkandnz09@gmail.com>
2024-12-07 15:58:44 +0300

    user contoller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const paswordEncypt = require("../helpers/passwordEncrypt");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -10,11 +11,13 @@ const UserSchema = new mongoose.Schema(
       unique: true,
       required: [true, "Email is required"],
     },
-    //!password alanı database e direkt kaydedilmez
+    //!password alanı database e direkt kaydedilmez bu nedenle hash fonksiyonu ile hashing yapılır(helper doyasında mevcut)
     password: {
       type: String,
       trim: true,
       required: [true, "Email is required"],
+      set: (password)=>paswordEncypt(password)
+      //set:paswordEncypt buradaki kullanım adresini (paswordEncypt) temsil eder
     },
     firstname: String,
     lastname: String,
