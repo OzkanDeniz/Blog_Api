804278e381a0f894e4474fea7f92d5ad0df05c07
ozkan <ozkandnz09@gmail.com>
2024-12-07 17:07:08 +0300

    validate email

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,13 +10,34 @@ const UserSchema = new mongoose.Schema(
       trim: true,
       unique: true,
       required: [true, "Email is required"],
+
+      //!1.yöntem
+      // validate: (email) => {
+      //   return email.includes("@") && email.includes(".");
+      // },
+      //!2.yöntem
+      // validate: (email)=>email.includes("@") && email.includes(".")
+      //!3.yöntem 
+      validate: [
+        (email) => {
+          return email.includes("@") && email.includes(".");
+        },
+        "Email format is incorrect",
+      ],
+      //!4.yöntem
+      // validate:{
+      //   validator:(email)=>{
+      //     return email.include("@") && email.includes(".");
+      //   },
+      //   message:"Email format is incorrect"
+      // }
     },
     //!password alanı database e direkt kaydedilmez bu nedenle hash fonksiyonu ile hashing yapılır(helper doyasında mevcut)
     password: {
       type: String,
       trim: true,
       required: [true, "Email is required"],
-      set: (password)=>paswordEncypt(password)
+      set: (password) => paswordEncypt(password),
       //set:paswordEncypt buradaki kullanım adresini (paswordEncypt) temsil eder
     },
     firstname: String,
