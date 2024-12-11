bf119f89eed93dc7c0cd2316bb2aaa677fdaa654
ozkan <ozkandnz09@gmail.com>
2024-12-11 15:19:33 +0300

    login logout session cookie check user data from session

--- main.js
+++ main.js
@@ -35,36 +35,7 @@ app.use(
 /*-------------------------------------------*/
 //(15) Check user-data from session:
 
-// const { User } = require("./src/models/user.model");
-
-// app.use(async (req, res, next) => {
-
-//   console.log("Session", req.session);
-
-//   // login olan user datasını buraya kaydedeceğiz.
-//   req.user = null
-
-//   if (req.session?._id) {
-//     //session da data varmı eğer varsa
-
-//     const { _id, password } = req.session; //varsa bu dataları al
-
-//     const user = await User.findOne({ _id }); // bu _id userı kontrol et
-
-//     if (user && user.password == password) {
-//       //Login Başarılı!
-//       // bu user veri tabanında varmı? && kullanıcı parolasıyla cookie deki parola aynımı değiştirmiş olabilir!
-//       req.user = user; //Session içindeki login olmuş olan user datası başarılı ise user versini req.user'a ata.
-//     } else {
-//       //kullanıcıyı bulamıyor ve ya passwordu doğrulayamıyorsa o zaman session daki datayı sil
-//       req.user=null
-//       req.session = null;
-//     }
-//   }
-
-//   next();
-// });
-//!useControl middleware e taşındı
+//Moved to file:
 app.use(require("./src/middlewares/userControl"))
 
 /*-------------------------------------------*/
